42e20dff103ecf186d5fb53f1ca024498454e73c
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-03-29 20:45:30 +0300

    test 2

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ function Error() {
 const express = require("express")
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 app.get('/location', (request, response, next) => {
